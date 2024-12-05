5620895809a8e8111253b7d564855fa20f4bdc1f
MrJack357 <thiaginho357@gmail.com>
2024-12-05 19:45:23 -0300

    aula de desestruturaçao

---
+++ index.js
@@ -0,0 +1,24 @@
+let pessoa = { nome: 'Thiago', sobrenome: 'Silva', idade: 27 }
+// let nome = pessoa.nome
+// let idade = pessoa.idade
+
+// console.log(nome);
+// console.log(idade);
+
+// let {nome, idade , sobrenome } = pessoa
+
+// console.log(nome);
+// console.log(idade);
+// console.log(sobrenome);
+
+const numeros = [1, 2, 3, 4];
+
+const [um, dois, tres, quatro] = numeros
+
+console.log(um);
+
+console.log(dois);
+
+console.log(tres);
+
+console.log(quatro);
